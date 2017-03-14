0d712e3af78b24e75c099e7afe993b4ca3b1e6d3
Amoshalt <tardy.m02@gmail.com>
2017-03-14 15:04:15 +0100

    Debut plateauTetris

---
+++ src/Plateau/PlateauTetris.java
@@ -0,0 +1,20 @@
+package Plateau;
+
+import GrilleJeux.Grille;
+import Tetriminos.Piece;
+
+/**
+ * Created by Martial TARDY on 14/03/2017.
+ */
+public class PlateauTetris {
+    private Grille grille;
+    private int Score;
+    private Piece pieceActu;
+    private Piece pieceSuiv;
+
+    public PlateauTetris(Grille grille, Piece pieceActu, Piece pieceSuiv) {
+        this.grille = grille;
+        this.pieceActu = pieceActu;
+        this.pieceSuiv = pieceSuiv;
+    }
+}
