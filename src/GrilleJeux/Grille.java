c42b1d3d647353a634717538426db14bb7b6639b
Amoshalt <tardy.m02@gmail.com>
2017-03-14 14:17:12 +0100

    fonction suppLigne final

--- src/GrilleJeux/Grille.java
+++ src/GrilleJeux/Grille.java
@@ -2,6 +2,7 @@ package GrilleJeux;
 
 import GrilleJeux.Case;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -12,6 +13,13 @@ public class Grille {
 
     public Grille() {
         this.grille = new Case[10][10];
+        for (int i = 0 ; i< this.grille.length;i++)
+        {
+            for (int j = 0 ; j< this.grille[i].length;j++)
+            {
+                this.grille[i][j] = new Case(true,i,j);
+            }
+        }
     }
 
     public Case[][] getGrille() {
@@ -32,21 +40,118 @@ public class Grille {
         int i=0,j=0;
         ArrayList tabLignePleines = new ArrayList();
         Case[][] g = this.grille;
-        boolean forme= true;
-        while (i<g.length)
+        boolean ligne= true;
+        for(i=0;i<g.length;i++)
         {
-            forme= true;
-            while ((j<g[i].length) && forme)
+            ligne= true;
+            while ((j<g[i].length) && ligne)
             {
-                
+                if (g[i][j].isVide())
+                {
+                    ligne = false;
+                }
                 j++;
             }
-            if (forme)
+            if (ligne)
             {
                 tabLignePleines.add(i);
+                System.out.println(i);
+
             }
-            i++;
+            j = 0;
         }
         return tabLignePleines;
+    }
+
+    public void suppLignes()
+    {
+        ArrayList ligneASupp = EvaluerGrille();
+        Case[][] g = this.grille;
+        int numLigne = 0;
+        int numArray = ligneASupp.size()- 1;
+        int nbLigne = 0;//1; //nombre courant du décalage
+        if (ligneASupp.size() >= 1)
+        {
+            numLigne = (int) ligneASupp.get(numArray);
+            //nbLigne = 9 - numLigne;
+            for (int i = g.length - 1; i >= 0; i--)
+            {
+                System.out.println("i: " + i +" nbLigne: " + nbLigne + " numLigne: " + numLigne + " numArray: " + numArray);
+                if (i != numLigne)
+                {
+                    if(nbLigne != 0)
+                    {
+                        for (int j =g[i].length - 1 ; j >= 0; j--)
+                        {
+                            g[i+nbLigne][j] = new Case(g[i][j].isVide(),i+nbLigne,j);
+                            g[i][j].setVide(true);
+                        }
+                    }
+
+                    affichageGrilleConsole();
+
+
+                }
+                else
+                {
+
+                    System.out.println("On a trouvé une ligne pleine");
+                    for (int j =g[i].length - 1 ; j >= 0; j--)
+                    {
+                        g[i][j].setVide(true);
+                    }
+                    if (numArray > 0)
+                    {numArray--;}
+                    nbLigne++;
+                    numLigne = (int) ligneASupp.get(numArray);
+                }
+
+
+
+            }
+        }
+
+    }
+
+    public void affichageGrilleConsole()
+    {
+        Case[][] g = this.getGrille();
+
+        for (int i = 0; i < g.length; i++)
+        {
+            for (int j = 0 ; j <g[i].length ; j++) {
+
+                if (g[i][j].isVide())
+                {
+                    System.out.print(" v ");
+                }
+                else
+                {
+                    System.out.print(" p ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public void affichageGrilleConsole2()
+    {
+        Case[][] g = this.getGrille();
+
+        for (int i = 0; i < g.length; i++)
+        {
+            for (int j = 0 ; j <g[i].length ; j++) {
+
+                System.out.print(" " +g [i][j].getX());
+                System.out.print(g [i][j].getY() + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
     }
 }
--- src/Main/main.java
+++ src/Main/main.java
@@ -1,5 +1,6 @@
 package Main;
 import Fenêtre.*;
+import GrilleJeux.Grille;
 
 /**
  * Created by Brandon on 14/02/2017.
@@ -9,7 +10,28 @@ public class main {
 
     public static void main(String[] args) {
 
-        FenetrePrincipale fenetrePrincipale = new FenetrePrincipale();
+        Grille g = new Grille();
+
+        g.affichageGrilleConsole();
+        for (int j = 0; j < g.getGrille()[1].length; j++)
+        {
+
+            g.getGrille()[9][j].setVide(false);
+
+            g.getGrille()[5][j].setVide(false);
+            g.getGrille()[0][j].setVide(false);
+        }
+
+        g.getGrille()[5][6].setVide(true);
+        g.getGrille()[6][0].setVide(false);
+        g.getGrille()[7][1].setVide(false);
+        g.getGrille()[8][2].setVide(false);
+        g.getGrille()[9][3].setVide(false);
+
+        g.affichageGrilleConsole();
+        g.suppLignes();
+        g.affichageGrilleConsole();
+        g.affichageGrilleConsole2();
 
     }
 
