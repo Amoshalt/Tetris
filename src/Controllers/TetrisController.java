39745edeefdddcbfe84208fc8367a14062986485
Piard Brandon <pgm-27@live.fr>
2017-03-21 13:05:56 +0100

    Debug via Cherry-pick (récupération de code perdu)

--- src/Controllers/TetrisController.java
+++ src/Controllers/TetrisController.java
@@ -1,8 +1,49 @@
 package Controllers;
 
+import Fenêtre.FenetreTetris;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 /**
  * Created by Brandon on 14/03/2017.
  */
 public class TetrisController {
+
+    private FenetreTetris fenetreTetris;
+
+    EventHandler<KeyEvent> listener;
+
+    public TetrisController(FenetreTetris fenetreTetris){
+        this.fenetreTetris = fenetreTetris;
+
+        listener = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode().equals(KeyCode.RIGHT)){
+                    bougerDroite();
+                }
+                if(event.getCode().equals(KeyCode.LEFT)){
+                    bougerGauche();
+                }
+                if(event.getCode().equals(KeyCode.DOWN)){
+                    descendre();
+                }
+            }
+        };
+
+    }
+
+    public void bougerDroite(){
+
+    }
+
+    public void bougerGauche(){
+
+    }
+
+    public void descendre(){
+
+    }
 
 }
